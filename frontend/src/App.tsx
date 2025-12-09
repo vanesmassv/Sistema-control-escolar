a8b437838b7cfca1e83d276303cbea0cf01aaca4
vanesmassv <jorge23borneo@gmail.com>
2025-12-09 11:38:32 -0700

    correcion de direccionamiento de ruta

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route 
             path="/login" 
             element={
